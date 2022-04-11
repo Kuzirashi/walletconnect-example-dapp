c5ee8b395834479b72cb3a22b55de9cde280f08f
Daniel Kmak <neverkuzi@gmail.com>
2022-04-11 17:07:31 +0200

    pages

---
+++ build/precache-manifest.9d61d4c4b2d02a784e102a266500324b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78ba86d06cfb69e9750b",
+    "url": "/static/js/main.78ba86d0.chunk.js"
+  },
+  {
+    "revision": "abee724d363a48d612b9",
+    "url": "/static/js/1.abee724d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e1cb8d21bffbcc2a8c7bfcf95bad12ff",
+    "url": "/static/media/walletconnect-banner.e1cb8d21.png"
+  },
+  {
+    "revision": "906078994f7221a25b900f47425b3ecc",
+    "url": "/static/media/erc20.90607899.svg"
+  },
+  {
+    "revision": "42bca7317690c3f5e9ecd8db387344df",
+    "url": "/static/media/eth.42bca731.svg"
+  },
+  {
+    "revision": "e37299a879ceb5a23000d8a2bb10bc46",
+    "url": "/static/media/xdai.e37299a8.png"
+  },
+  {
+    "revision": "d88790e743e3917838dc6642e2d98768",
+    "url": "/index.html"
+  }
+];
